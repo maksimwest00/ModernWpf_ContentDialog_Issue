daf8a13118be0a1a1d74cf11eb26457ef48a649d
maksimwest00 <maksim.west00@yandex.ru>
2022-09-02 19:23:17 +0300

    Добавьте файлы проекта.

---
+++ ModernWpf_ContentDialog_Issue/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ModernWpf.Controls;
+using System.Windows;
+
+namespace ModernWpf_ContentDialog_Issue
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ContentDialog contentDialog = new ContentDialog();
+            ModernWpf.ThemeManager.SetRequestedTheme(contentDialog, ModernWpf.ElementTheme.Dark);
+            contentDialog.Title = "Hello";
+            contentDialog.Content = "Welcome";
+            contentDialog.CloseButtonText = "Exit";
+            contentDialog.ShowAsync();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            childwindow childwindow = new childwindow(this);
+            childwindow.Show();
+        }
+    }
+}
---
+++ ModernWpf_ContentDialog_Issue/childwindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ModernWpf.Controls;
+using System.Windows;
+
+namespace ModernWpf_ContentDialog_Issue
+{
+    public partial class childwindow : Window
+    {
+        public childwindow(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.Owner = mainWindow;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ContentDialog contentDialog = new ContentDialog();
+            ModernWpf.ThemeManager.SetRequestedTheme(contentDialog, ModernWpf.ElementTheme.Dark);
+            contentDialog.Title = "Hello";
+            contentDialog.Content = "Welcome";
+            contentDialog.CloseButtonText = "Exit";
+            contentDialog.ShowAsync();
+        }
+    }
+}
